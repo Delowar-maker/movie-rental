b4d2e0b2af31dd2e0420482741326941950db447
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-06-05 20:39:07 +0600

    header sidebar 1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
 export default function App() {
   return (
-    <div>
-      <p className="text-sky-400/100">The quick brown fox...</p>
-    </div>
+    <>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+        </div>
+      </main>
+    </>
   );
 }
