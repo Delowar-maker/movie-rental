0c32f0e40465397316c112ca0fa247eed02da0ef
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-06-07 13:14:59 +0600

    implementing Dark mode use

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import { useState } from "react";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
-import MovieList from "./cine/MovieList";
-import { MovieContext } from "./context";
+import Page from "./Page";
+import { MovieContext, ThemeContext } from "./context";
 
 export default function App() {
-
   const [cartData, setCartData] = useState([]);
+  const [darkMode, setDarkMode] = useState(true);
   return (
     <>
-      <MovieContext.Provider value={{ cartData, setCartData }}>
-        <Header />
-        <main>
-          <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-            <Sidebar />
-            <MovieList />
-          </div>
-        </main>
-      </MovieContext.Provider>
+      <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+        <MovieContext.Provider value={{ cartData, setCartData }}>
+          <Page />
+        </MovieContext.Provider>
+      </ThemeContext.Provider>
     </>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,29 +1,28 @@
 import { useContext, useState } from "react";
 import Moon from "./assets/icons/moon.svg";
+import Sun from "./assets/icons/sun.svg";
 import Logo from "./assets/logo.svg";
 import Ring from "./assets/ring.svg";
 import ShoppingCart from "./assets/shopping-cart.svg";
 import CartDetails from "./cine/CartDetails";
-import { MovieContext } from "./context";
+import { MovieContext, ThemeContext } from "./context";
 export default function Header() {
-
   const [showCart, setShowCart] = useState(false);
 
   // context using
 
   const { cartData } = useContext(MovieContext);
-  console.log(cartData);
+
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
 
   // function implementation
 
   const handleCartShow = () => {
     setShowCart(true);
-  }
+  };
   return (
     <header>
-      {
-        showCart && <CartDetails onClose={() => setShowCart(false)} />
-      }
+      {showCart && <CartDetails onClose={() => setShowCart(false)} />}
       <nav className="container flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
           <img src={Logo} width="139" height="26" alt="" />
@@ -42,8 +41,9 @@ export default function Header() {
             <a
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
+              onClick={() => setDarkMode(!darkMode)}
             >
-              <img src={Moon} width="24" height="24" alt="" />
+              <img src={darkMode ? Sun : Moon} width="24" height="24" alt="" />
             </a>
           </li>
           <li>
@@ -52,14 +52,12 @@ export default function Header() {
               href="#"
               onClick={handleCartShow}
             >
-              <img
-                src={ShoppingCart}
-                width="24"
-                height="24"
-                alt="" />
-              {
-                cartData.length > 0 && <span className="absolute -top-2 -right-2 bg-primary text-white rounded-full w-6 h-6 flex items-center justify-center">{cartData.length}</span>
-              }
+              <img src={ShoppingCart} width="24" height="24" alt="" />
+              {cartData.length > 0 && (
+                <span className="absolute -top-2 -right-2 bg-primary text-white rounded-full w-6 h-6 flex items-center justify-center">
+                  {cartData.length}
+                </span>
+              )}
             </a>
           </li>
         </ul>
